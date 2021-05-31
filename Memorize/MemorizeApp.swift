3776dcceb58df700af87ca66f54af1ccb51cad02
Egor Badmaev <badmaeve2511@gmail.com>
2021-05-31 19:22:43 +0300

    init: start of the project 'Memorize'

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Егор Бадмаев on 28.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
