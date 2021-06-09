5d258dcf98b2d50d905769a9c57ec9031d71e2e6
Egor Badmaev <badmaeve2511@gmail.com>
2021-06-09 16:19:27 +0300

    feat: fast counter project

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,25 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0;
+    
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
-        }
-        .padding(.horizontal)
-        .foregroundColor(/*@START_MENU_TOKEN@*/.orange/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct CardView: View {
-    var body: some View {
-        ZStack {
-            // Это функция
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 2.0)
-            Text("Hello, world!")
+        VStack {
+            Text("You have clicked \(count) times")
+            Button(action: {count += 1}) {
+                Text("Button")
+            }
         }
     }
 }
